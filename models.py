906e73610ba6bd461091b5fc020fa2966aac97c9
Sean Mullane <smullane@stanford.edu>
2018-05-10 23:34:04 -0700

    Started the models document... with a comment LOL

---
+++ models.py
@@ -0,0 +1,8 @@
+"""
+Models for electromagnetic particle shower classification. 
+	(1) FC Network --> Softmax
+	(2) 3 CNN --> FC network --> Softmax
+	(3) 3 CNN --> CNN ---> FC network --> Softmax 
+See Git Repo (https://github.com/nhartman94/CNN_Project) for more information.
+"""
+
